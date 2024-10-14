63a4bd0e1397a5e5dcabd8b0ee9825e0cc97d8bd
arraysort <dudwns1045@gmail.com>
2024-10-14 18:58:34 +0900

    feat : Spring Security PasswordEncoder 빈 등록 / 초기 페이지 비활성화
    1. Spring Security PasswordEncoder 빈 등록 / 초기 페이지 비활성화
     - 회원가입을 위한 PasswordEncoder 빈 등록
     - SecurityAutoConfiguration(초기 페이지) 비활성화

--- src/main/java/arraysort/project/yboard/YboardApplication.java
+++ src/main/java/arraysort/project/yboard/YboardApplication.java
@@ -2,10 +2,13 @@ package arraysort.project.yboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@SpringBootApplication
+@EnableJpaAuditing
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class YboardApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
